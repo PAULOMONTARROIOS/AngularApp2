52f94940787d9b4985d8ac64b197de075ba04ba6
Paulo Montarroios <pauloricardomontarroios@hotmail.com>
2018-01-30 23:09:56 -0200

    Validacoes iniciais dos formularios

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = ''
+  public numero: string = ''
+  public complemento: string = ''
+  public formaPagamento: string = ''
+
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public atualizaEndereco(endereco: string) {
+    this.endereco = endereco;
+
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true
+    } else {
+      this.enderecoValido = false
+    }
+  }
+
+  public atualizaNumero(numero: string) {
+    this.numero = numero;
+    let num = parseInt(this.numero)
+    if (num >= 0) {
+      this.numeroValido = true
+    } else {
+      this.numeroValido = false
+    }
+  }
+
+  public atualizaComplemento(complemento: string) {
+    this.complemento = complemento;
+
+    if (this.complemento.length > 0) {
+      this.complementoValido = true
+    }
+  }
+
+  public atualizaFormaPagamento(formaPagamento) {
+    this.formaPagamento = formaPagamento;
+
+    if (this.formaPagamento != "dinheiro" && this.formaPagamento != "debito") {
+      this.formaPagamentoValido = false
+    } else {
+      this.formaPagamentoValido = true
+    }
+  }
 }
